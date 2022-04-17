3b0266be590d28f0b488a8240f2cb7a573504da6
amoralesc <a.moralesc@javeriana.edu.co>
2022-04-16 22:05:00 -0500

    Add bus.event and aggregate root

--- realstate/src/main/java/com/webdev/realstate/shared/domain/aggregate/AggregateRoot.java
+++ realstate/src/main/java/com/webdev/realstate/shared/domain/aggregate/AggregateRoot.java
@@ -1,4 +1,23 @@
 package com.webdev.realstate.shared.domain.aggregate;
 
-public class AggregateRoot {
+
+import com.webdev.realstate.shared.domain.bus.event.DomainEvent;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public abstract class AggregateRoot {
+
+	private List<DomainEvent> domainEvents = new ArrayList<>();
+
+	final public List<DomainEvent> pullDomainEvents() {
+		List<DomainEvent> events = domainEvents;
+		domainEvents = Collections.emptyList();
+		return events;
+	}
+
+	final protected void record(DomainEvent event) {
+		domainEvents.add(event);
+	}
 }
